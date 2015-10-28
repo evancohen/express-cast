6e2d250f029dbf50e8944c79aedea8ac7b1af3fc
Evan Cohen <evanbtcohen@gmail.com>
2015-10-27 18:17:18 -0700

    updagint readme and config

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # express-cast
 A local HTTP server running node that exposes Chromecast functionality
+
+## Getting started
+In order to use Google Music you need to get a key first. If you use 2-Factor Authentication (which you absolutly should) then you can set up a temporary (App Password)[https://security.google.com/settings/security/apppasswords]
+
+Once you set that up, fill out `login.js` with your info and then run it with
+```
+node login.js
+```
+If everything works as it should you should get an Android ID and a Master Token which you stick in your config.
+
+That's it, you are ready to go!
--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 //Simple server that exposes your Chromecast devices over HTTP to make home automation easier
 
+var express = require('express');
 var PlayMusic = require('playmusic');
 var config = require('./config');
-var pm = new PlayMusic();
 
-pm.init({email: config.gmusic.email, password: config.gmusic.email}, function(err) {
-    if(err) console.error(err);
-    // Something has gone terribly wrong. Check your username and password
-    
+var pm = new PlayMusic();
+var app = express();
+
+//Initiate Google Muisc
+pm.init(config.gmusic, function(err, other) {
+  if(err) console.error(err);
+  // place code here
 })
+
+//GET on the root, yo
+app.get('/', function (req, res) {
+  res.send('Ready to do your bidding!');
+});
+
+app.get('/song', function (req, res) {
+  var response = {};
+  pm.getAllTracks(function(err, library) {
+    var song = library.data.items.pop();
+    response.song = song;
+    pm.getStreamUrl(song.id, function(err, streamUrl) {
+        response.streamUrl = streamUrl;
+        res.send(response);
+    });
+  });
+});
+
+
+
+//Listen on desired port 
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Ready to do your bidding at http://%s:%s', host, port);
+});
